b739468837a0b7ef186945a61222e308faf94da6
Shoichi Ishida <ishida@toeitechno.com>
2024-01-02 08:06:20 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RiverWaterSurface
+
+Developed with Unreal Engine 5
---
+++ Source/RiverWaterSurface/RiverWaterSurface.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RiverWaterSurface.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RiverWaterSurface, "RiverWaterSurface" );
